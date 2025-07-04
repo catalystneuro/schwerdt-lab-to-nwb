e2246e12ce237c8f3346942491f3a9ac6b64facc
weiglszonja <wszonja@gmail.com>
2025-07-04 13:00:37 +0200

    Add initial ecephys metadata

--- src/schwerdt_lab_to_nwb/amjad_2025/convert_session.py
+++ src/schwerdt_lab_to_nwb/amjad_2025/convert_session.py
@@ -47,7 +47,7 @@ def session_to_nwb(
     conversion_options = dict()
 
     # Add Recording
-    source_data.update(dict(Recording=dict(folder_path=session_folder_path)))
+    source_data.update(dict(Recording=dict(folder_path=session_folder_path, es_key="electrical_series")))
     conversion_options.update(dict(Recording=dict(stub_test=stub_test)))
 
     # Add Sorting
@@ -71,6 +71,9 @@ def session_to_nwb(
     editable_metadata = load_dict_from_file(editable_metadata_path)
     metadata = dict_deep_update(metadata, editable_metadata)
 
+    # Update the ecephys metadata
+    metadata["Ecephys"] = editable_metadata["Ecephys"]
+
     metadata["Subject"]["subject_id"] = subject_id
     metadata["NWBFile"]["session_id"] = session_id
 
@@ -91,3 +94,16 @@ if __name__ == "__main__":
     stub_test = True
 
     session_to_nwb(session_folder_path=data_dir_path, nwb_folder_path=output_dir_path, stub_test=stub_test)
+
+    # Debugging output TODO: remove before finalizing
+    print(f"Conversion completed. NWB file saved to: {output_dir_path}")
+    # read the nwb file and check the metadata
+    import pandas as pd
+    from pynwb import NWBHDF5IO
+
+    pd.set_option("display.max_columns", None)
+    nwbfile_path = output_dir_path / "nwb_stub" / "sub-Monkey-T_ses-09262024.nwb"
+    with NWBHDF5IO(nwbfile_path, "r") as io:
+        nwbfile = io.read()
+        assert len(nwbfile.devices) == 1, "Expected one device in the NWB file."
+        assert len(nwbfile.electrode_groups) == 1, "Expected one electrode group in the NWB file."
