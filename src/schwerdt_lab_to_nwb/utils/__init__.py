d630a99c33e5875b693affaf0f1511340f82b407
weiglszonja <wszonja@gmail.com>
2025-10-16 11:09:13 +0200

    remove unused parts

--- src/schwerdt_lab_to_nwb/utils/__init__.py
+++ src/schwerdt_lab_to_nwb/utils/__init__.py
@@ -1,7 +1,5 @@
 from .utils import (
     convert_timestamps_to_relative_timestamps,
     convert_unix_timestamps_to_datetime,
-    fetch_relative_fscv_start_times_from_trlist,
     get_channel_index_from_lfp_file_path,
-    reconstruct_continuous_signal_from_trial_aligned_data,
 )
